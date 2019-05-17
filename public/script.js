ec28ba00baba2921a2686676c1b04e3e5d72f730
Louis fabrizio <louisfabrizio.student@careerdevs.com>
2019-05-17 00:50:26 +0000

    updated

--- public/script.js
+++ public/script.js
@@ -1,1 +1,10 @@
-console.log("script is working");
+function getUserInfo(){
+    const input = document.getElementById("userName").value;
+    
+    const url = "/showprofile/" + input;
+    axios.get(url)
+    .then(response => {
+        document.getElementById("userInfo").innerHTML = response.data;
+    })
+}
+
