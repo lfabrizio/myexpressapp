a7a20f27dc09699a82b9024a6ed7c7e4e0b26639
Louis fabrizio <louisfabrizio.student@careerdevs.com>
2019-05-18 15:43:30 +0000

    update

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
 const port = 8080
 
+app.use(bodyParser.json());
 app.use('/', express.static("public"));
 
 app.get('/', function (req, res) {
 res.send('Hello!!')
 })
 
-app.get('/api', function (req, res){
-    const userName = req.query.username;
-    const userId = req.query.Id;
-    const message = req.query.message;
+app.post('/api', function (req, res){
+    const userName = req.body.username;
+    const userId = req.body.id;
+    const message = req.body.message;
     console.log("userName");
     const reply = `${userName} with id of ${userId} is saying ${message}`
     res.send(reply);
